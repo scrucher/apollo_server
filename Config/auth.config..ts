8f7b729565abadc976c0c27ec5313cb1ef55c69a
scrucher <jhonsteph666@gmail.com>
2021-11-24 14:35:23 +0100

    store signup and Login

--- Config/auth.config..ts
+++ Config/auth.config..ts
@@ -1,4 +1,4 @@
-export const DbUrl = 'mongodb://localhost:27018/delivery';
+export const DbUrl = process.env.DB_URL || 'mongodb://localhost:27018/delivery' ;
 export const DbConnectionOptions = {
     directConnection: true,
     autoCreate: true,
